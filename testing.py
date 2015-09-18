f8679ebcdd9c144bd691e80aaf3386e5112de2dc
jeff Wyckoff <jokerswild663@gmail.com>
2015-09-18 16:20:06 -0700

    updating test file

--- testing.py
+++ testing.py
@@ -2,7 +2,10 @@ import orderText
 import sys
 import pandas as pd
 
+#printing package info
 print(sys.version)
 pd.show_versions(as_json=False)
 
-orderText.test()
+#test run of orderText Module
+unique_list=orderText.removeDuplicates(['first.xml','first.xml','second.xml','second.xml','third.xml'])
+print(unique_list)

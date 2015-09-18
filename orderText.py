730acc05e71866d0623bae3327a7d213e526c79c
jeff Wyckoff <jokerswild663@gmail.com>
2015-09-18 16:29:11 -0700

    adding readText function to orderText module

--- orderText.py
+++ orderText.py
@@ -6,3 +6,8 @@ def removeDuplicates(dataList):
   unique=set(dataList)
   newList=list(unique)
   return newList
+
+def readText(filePath):
+  fileObject=open(filePath,'r')
+  print(fileObject.read())
+  return

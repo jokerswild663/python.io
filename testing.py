b0ecfd5667fb95c80d68ae9dcb431f976e11c64e
jeff Wyckoff <jokerswild663@gmail.com>
2015-09-18 18:12:48 -0700

    adding sample for reading text file

--- testing.py
+++ testing.py
@@ -9,3 +9,4 @@ pd.show_versions(as_json=False)
 #test run of orderText Module
 unique_list=orderText.removeDuplicates(['first.xml','first.xml','second.xml','second.xml','third.xml'])
 print(unique_list)
+orderText.readText('sample.txt')

dce24baa3b75542effa7b22ab847ecd61e567e52
jeff Wyckoff <jokerswild663@gmail.com>
2015-09-21 14:16:57 -0700

    adding comments to describe module bahavior

--- orderText.py
+++ orderText.py
@@ -1,13 +1,21 @@
 def test():
+  """this is a test function to see module is being called"""
   print('hello')
   return
 
 def removeDuplicates(dataList):
+  """This removes duplicate strings from a list of strings supplied
+  input: list of strings
+  output: list of strings"""
   unique=set(dataList)
   newList=list(unique)
   return newList
 
 def readText(filePath):
+  """This reads all text from a text file.  
+  Removes whitespace and splits strings into list of strings based on delimeter ','
+  input: relative path of text file
+  output: list of strings"""
   dataFromFile=[]
   with open(filePath,'r') as fileObject:
     fileLines=fileObject.readlines()

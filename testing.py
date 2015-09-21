45ca3353009a442dc62cfd437efaac9abf648eb4
Jeff Wyckoff <jokerswild663@gmail.com>
2015-09-21 00:17:23 -0700

    updating testing.py to test readText.  Changing comment

--- testing.py
+++ testing.py
@@ -6,7 +6,10 @@ import pandas as pd
 print(sys.version)
 pd.show_versions(as_json=False)
 
-#test run of orderText Module
+#test run of orderText Function
 unique_list=orderText.removeDuplicates(['first.xml','first.xml','second.xml','second.xml','third.xml'])
 print(unique_list)
-orderText.readText('sample.txt')
+
+#test run of readText Function
+listInFile=orderText.readText('sample.txt')
+print(listInFile)
